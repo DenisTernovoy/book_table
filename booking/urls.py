cdafdcfa551db619934917362e9d98a5ecbce0e8
DenisTernovoy <denis.ternovoi@mail.ru>
2026-02-17 23:03:02 +0300

    Написаны шаблоны для главной страницы и для страницы 'О ресторане'

--- booking/urls.py
+++ booking/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from booking.apps import BookingConfig
-from booking.views import BookingView
+from booking.views import BookingView, AboutView
 
 app_name = BookingConfig.name
 
 urlpatterns = [
     path("", BookingView.as_view(), name="booking"),
+    path("about/", AboutView.as_view(), name="about"),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
 # Create your views here.
 class BookingView(TemplateView):
     template_name = "booking/main_page.html"
+
+
+class AboutView(TemplateView):
+    template_name = "booking/about.html"
